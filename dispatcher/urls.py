3b5b24632ef4e9a4d163a4c707e0d8a02b68dd5b
Elgrush <nikikarp@mail.ru>
2023-04-07 23:56:37 +0300

    Change name and add landmark requests added

--- dispatcher/urls.py
+++ dispatcher/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('authorisation/sign_in/', views.sign_in, name='sign_in'),
     path('authorisation/change_name/', views.change_name, name='change_name'),
     path('landmarks/get_landmark/', views.get_landmark, name='get_landmark'),
-    path('landmarks/get_landmark/', views.suggest_landmark, name='suggest_landmark')
+    path('landmarks/suggest_landmark/', views.suggest_landmark, name='suggest_landmark')
 ]
